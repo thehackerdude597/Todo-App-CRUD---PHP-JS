14b5702165f57ef20433957b7a1fcaf26dc058fe
thehackerdude597 <78941180+thehackerdude597@users.noreply.github.com>
2021-11-19 17:48:51 -0500

    Create CRUDAction.js

---
+++ CRUDAction.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+   $("#1").on("click", function(){
+      $("#1").animate({opacity: "0.0"}, 500);
+      $("#usereward").animate({opacity: "1.0", fontSize: "25px", color: "white"}, 1500);
+   });
+
+
+    $("#2").on("click", function(){
+      $("#2").animate({opacity: "0.0"}, 500);
+      $("#usereward").animate({opacity: "1.0", fontSize: "25px", color: "white"}, 1500);
+   });
+
+
+
+    $("#3").on("click", function(){
+      $("#3").animate({opacity: "0.0"}, 500);
+      $("#usereward").animate({opacity: "1.0", fontSize: "25px", color: "white"}, 1500);
+   });
+
+
+
+    $("#4").on("click", function(){
+      $("#4").animate({opacity: "0.0"}, 500);
+      $("#usereward").animate({opacity: "1.0", fontSize: "25px", color: "white"}, 1500);
+   });
+
+
+
+    $("#5").on("click", function(){
+      $("#5").animate({opacity: "0.0"}, 500);
+      $("#usereward").animate({opacity: "1.0", fontSize: "25px", color: "white"}, 1500);
+   });
+
+
+   
+});
